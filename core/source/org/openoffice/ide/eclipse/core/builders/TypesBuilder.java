96767d2c3cd9387c09c555de496facf5316cbe54
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2015-12-08 09:14:33 +0100

    Remove unused imports & other cleanup

--- core/source/org/openoffice/ide/eclipse/core/builders/TypesBuilder.java
+++ core/source/org/openoffice/ide/eclipse/core/builders/TypesBuilder.java
@@ -106,7 +106,6 @@ public class TypesBuilder extends IncrementalProjectBuilder {
     /**
      * {@inheritDoc}
      */
-    @SuppressWarnings("unchecked")
     protected IProject[] build(int pKind, Map pArgs, IProgressMonitor pMonitor) throws CoreException {
         
         mChangedIdl = false;
