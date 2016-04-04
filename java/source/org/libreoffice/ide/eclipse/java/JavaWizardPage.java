13d789109a03047228c4430d13d9cc854c5467ab
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2016-04-04 16:15:21 +0200

    FTW: Don't generate tests by default

--- java/source/org/libreoffice/ide/eclipse/java/JavaWizardPage.java
+++ java/source/org/libreoffice/ide/eclipse/java/JavaWizardPage.java
@@ -132,7 +132,7 @@ public class JavaWizardPage extends LanguageWizardPage {
         // Create the test row
         mJavaTestsRow = new BooleanRow(body, JAVA_TESTS,
             Messages.getString("JavaWizardPage.IncludeTestClasses")); //$NON-NLS-1$
-        mJavaTestsRow.setValue(true);
+        mJavaTestsRow.setValue(false);
         mJavaTestsRow.setFieldChangedListener(new IFieldChangedListener() {
 
             @Override
