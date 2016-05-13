e48f5d45843b643f1787c1d4e3b95beecf1e7e02
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2016-05-13 10:22:34 +0200

    Unused imports

--- java/source/org/libreoffice/ide/eclipse/java/JavaBuilder.java
+++ java/source/org/libreoffice/ide/eclipse/java/JavaBuilder.java
@@ -45,8 +45,6 @@ package org.libreoffice.ide.eclipse.java;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.LineNumberReader;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 
