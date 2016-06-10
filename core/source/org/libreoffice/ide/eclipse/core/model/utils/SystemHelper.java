f933ed99877abeb60da69b487e486d485159dea4
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2016-06-10 12:41:14 +0200

    Unused imports

--- core/source/org/libreoffice/ide/eclipse/core/model/utils/SystemHelper.java
+++ core/source/org/libreoffice/ide/eclipse/core/model/utils/SystemHelper.java
@@ -43,10 +43,8 @@
  ************************************************************************/
 package org.libreoffice.ide.eclipse.core.model.utils;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Map.Entry;
