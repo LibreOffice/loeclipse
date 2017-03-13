LibreOffice/loeclipse

commit 9998b1c4844ef645af6e2552f6d48da0b6c87873

Fix #6: Multiple unopkgs in PATH

core/source/org/libreoffice/ide/eclipse/core/internal/model/OOo.java
@@ -386,7 +386,8 @@ public class OOo extends AbstractOOo {
         String path = pPackageFile.getAbsolutePath();
         String shellCommand = MessageFormat.format("unopkg gui -f \"{0}\"", path); //$NON-NLS-1$
 
-        String[] env = SystemHelper.getSystemEnvironement();
+        // Don't get the system env variables - the $PATH might contain other `unopkg`s which we don't want.
+        String[] env = new String[]{};
         String pathsep = System.getProperty("path.separator"); //$NON-NLS-1$
         env = SystemHelper.addEnv(env, "PATH", getHome() + FILE_SEP + "program", pathsep); //$NON-NLS-1$ //$NON-NLS-2$
         env = addUserProfile(pUserInstallation, env);

commit a202905cfc1c0a2494c24523c2a4c26e17c72fe0

Fix #55 Extension is not deployed on Ubuntu
Regression since 9998b1c

core/source/org/libreoffice/ide/eclipse/core/internal/model/OOo.java
@@ -384,8 +384,8 @@ public class OOo extends AbstractOOo {
         String path = pPackageFile.getAbsolutePath();
         String shellCommand = MessageFormat.format("unopkg add -f \"{0}\"", path); //$NON-NLS-1$
 
-        // Don't get the system env variables - the $PATH might contain other `unopkg`s which we don't want.
-        String[] env = new String[]{};
+        // We need system env variables - at least on Linux the unopkg is in the global path, but not in instdir/program
+        String[] env = SystemHelper.getSystemEnvironement();
         String pathsep = System.getProperty("path.separator"); //$NON-NLS-1$
         env = SystemHelper.addEnv(env, "PATH", getHome() + FILE_SEP + "program", pathsep); //$NON-NLS-1$ //$NON-NLS-2$
         env = addUserProfile(pUserInstallation, env);

core/source/org/libreoffice/ide/eclipse/core/model/utils/SystemHelper.java
@@ -163,7 +163,10 @@ public class SystemHelper {
                 result = new String[pEnv.length];
                 System.arraycopy(pEnv, 0, result, 0, pEnv.length);
                 if (null != pSeparator) {
-                    result[i] = pEnv[i] + pSeparator + pValue;
+                    // First remove the leading NAME=
+                    String tmpEnv =  pEnv[i].replaceFirst(pName+"=", "");
+                    // Put the new env in front of the existing one
+                    result[i] = pName + "=" + pValue + pSeparator + tmpEnv;
                 } else {
                     result[i] = pName + "=" + pValue; //$NON-NLS-1$
                 }